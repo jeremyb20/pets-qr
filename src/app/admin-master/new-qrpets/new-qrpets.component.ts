9c93bc5cf1936c5c2e2a9d99e22b558b4f9b16a5
Jeremy <jbarquero@cog.cr>
2021-04-30 13:26:55 -0600

    Remove LiI Ramdom

--- src/app/admin-master/new-qrpets/new-qrpets.component.ts
+++ src/app/admin-master/new-qrpets/new-qrpets.component.ts
@@ -108,7 +108,7 @@ export class NewQRPetsComponent implements OnInit {
   
   makeid(length) {
     var result = [];
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
       result.push(characters.charAt(Math.floor(Math.random() *
