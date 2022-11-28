8376622a6dfc5245cb73d65ffc86701369852592
Jeremy Bacca Barquero <jbaccab@ucenfotec.ac.cr>
2022-11-28 10:00:44 -0600

    check v2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   environmentName: 'dev',
   //ws: 'https://www.localpetsandfamily.com'
-  //ws: 'https://localpetsandfamily.onrender.com'
-  ws: 'https://petsqrbackend.fly.dev'
+  ws: 'https://localpetsandfamily.onrender.com'
+  //ws: 'https://petsqrbackend.fly.dev'
 
 };
 
